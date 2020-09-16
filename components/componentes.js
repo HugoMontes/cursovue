12bde5626a71c6fb36ebd38bff2689a93866d4c2
hmontes <hmontes@cofar.com.bo>
2020-09-16 16:51:52 -0400

    Ocultar logica de vuex para obtener un valor mediante un propiedad calculada

--- components/componentes.js
+++ components/componentes.js
@@ -1,11 +1,20 @@
 // Crear un componente titulo
 // Visualizar valor de numero usando vuex 
 Vue.component('padre', {
+    // Invocar a una propiedad calculada
     template: 
     `<div>
-        <h1>numero {{ $store.state.numero }}</h1>
+        <h1>numero {{ numero }}</h1>
         <hijo></hijo>
-    </div>`
+    </div>`,
+    // Crear una propiedad calculada para retornar
+    // unicamente el valor de numero
+    computed: {
+        numero: function() {
+            // Retorna unicamente el valor requerido
+            return store.state.numero;
+        }
+    }
 })
 
 // Crear un componente hijo
